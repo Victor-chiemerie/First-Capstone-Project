fa910293eecd5a3d680db007c61d7e74174aea3b
Victor-chiemerie <victorchiemerie02@gmail.com>
2023-01-03 17:30:58 +0100

    Fixed linters erros on css and javascript files

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ menuLinks.forEach((link) => {
 
 const speakers = document.querySelector('#speakers');
 const FeaturedSpeaker = document.createElement('div');
-const ul_list = document.createElement('ul');
+const ulList = document.createElement('ul');
 
 FeaturedSpeaker.innerHTML = `
         <h2 class="text-dark">Featured Speakers</h2>
@@ -28,58 +28,58 @@ FeaturedSpeaker.innerHTML = `
     `;
 
 speakers.appendChild(FeaturedSpeaker);
-speakers.appendChild(ul_list);
+speakers.appendChild(ulList);
 
 const presentation = document.querySelector('#speakers ul');
 
 const presenters = [
-    {
-        id: 1,
-        name: 'Nwadinigwe Victor',
-        qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
-        profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
-        image: 'images/profile.jpg'
-    },
-    {
-        id: 2,
-        name: 'Oguzie-Ibeh Richard',
-        qualification: 'Director of Art CEntre Nabi and a board member of CC Korea',
-        profession: 'AS the main venue for new media art production in Korea, Nabi promotes cross-disiplinary collaboration and understanding among science technology, humanities, and the arts.',
-        image: 'images/profile.jpg'
-    },
-    {
-        id: 3,
-        name: 'Ebolom Mcsaint',
-        qualification: 'Executive director of the Wikimedia Foundation',
-        profession: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikimedia. Wikimedia is freely available in 290 languages and used by nearly half a billion people around the world every month.',
-        image: 'images/profile.jpg'
-    },
-    {
-        id: 4,
-        name: 'John Collins',
-        qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
-        profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
-        image: 'images/profile.jpg'
-    },
-    {
-        id: 5,
-        name: 'Edom Nancy',
-        qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
-        profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
-        image: 'images/profile.jpg'
-    },
-    {
-        id: 6,
-        name: 'Akachukwu Smart',
-        qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
-        profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
-        image: 'images/profile.jpg'
-    }
+  {
+    id: 1,
+    name: 'Nwadinigwe Victor',
+    qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
+    image: 'images/profile.jpg',
+  },
+  {
+    id: 2,
+    name: 'Oguzie-Ibeh Richard',
+    qualification: 'Director of Art CEntre Nabi and a board member of CC Korea',
+    profession: 'AS the main venue for new media art production in Korea, Nabi promotes cross-disiplinary collaboration and understanding among science technology, humanities, and the arts.',
+    image: 'images/profile.jpg',
+  },
+  {
+    id: 3,
+    name: 'Ebolom Mcsaint',
+    qualification: 'Executive director of the Wikimedia Foundation',
+    profession: 'Lila Tretikov is the Executive Director of the Wikimedia Foundation, the nonprofit organization that operates Wikimedia. Wikimedia is freely available in 290 languages and used by nearly half a billion people around the world every month.',
+    image: 'images/profile.jpg',
+  },
+  {
+    id: 4,
+    name: 'John Collins',
+    qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
+    image: 'images/profile.jpg',
+  },
+  {
+    id: 5,
+    name: 'Edom Nancy',
+    qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
+    image: 'images/profile.jpg',
+  },
+  {
+    id: 6,
+    name: 'Akachukwu Smart',
+    qualification: 'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    profession: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006.',
+    image: 'images/profile.jpg',
+  },
 ];
 presenters.forEach((presenter) => {
-    const list = document.createElement('li');
-    list.classList.add(`presenter${presenter.id}`)
-    list.innerHTML = `
+  const list = document.createElement('li');
+  list.classList.add(`presenter${presenter.id}`);
+  list.innerHTML = `
         <div class="speaker-image">
             <div class="speaker-bg"></div>
             <img src="${presenter.image}" alt="">
@@ -91,7 +91,7 @@ presenters.forEach((presenter) => {
             <p>${presenter.profession}</p>
         </div>
     `;
-    presentation.appendChild(list);
+  presentation.appendChild(list);
 });
 
 // See Program
@@ -104,24 +104,24 @@ const seeDesktop = document.querySelector('#see-program');
 // const speakersList = document.querySelectorAll('#speakers-list');
 
 function toggleSeeMobile() {
-    speakers.classList.toggle('hidden');
-    partners.classList.toggle('hidden');
-    footer.classList.toggle('hidden');
-    seeLess.classList.toggle('hidden');
-    seeMore.classList.toggle('hidden');
-    // speakersList.forEach((list) => {
-    //     list.classList.toggle('hidden');
-    // });
+  speakers.classList.toggle('hidden');
+  partners.classList.toggle('hidden');
+  footer.classList.toggle('hidden');
+  seeLess.classList.toggle('hidden');
+  seeMore.classList.toggle('hidden');
+  // speakersList.forEach((list) => {
+  //     list.classList.toggle('hidden');
+  // });
 }
 
 function toggleSeeDesktop() {
-    // speakersList.forEach((list) => {
-    //     list.classList.toggle('hidden');
-    // });
-    speakers.classList.remove('hidden');
-    partners.classList.remove('hidden');
-    footer.classList.remove('hidden');
-    seeDesktop.classList.add('hidden');
+  // speakersList.forEach((list) => {
+  //     list.classList.toggle('hidden');
+  // });
+  speakers.classList.remove('hidden');
+  partners.classList.remove('hidden');
+  footer.classList.remove('hidden');
+  seeDesktop.classList.add('hidden');
 }
 
 seeLess.addEventListener('click', toggleSeeMobile);
